f7d43e1cd05405fd6659293684bfe0b03e5dfc3b
Roshan Singh <ravi.roshan04@gmail.com>
2022-05-11 00:22:43 +0530

    basic skeleton of func in controller added

---
+++ controllers/address.go
@@ -0,0 +1,17 @@
+package controllers
+
+func AddAddress() gin.HandlerFunc{
+
+}
+
+func EditHomeAddress() gin.HandlerFunc{
+
+}
+
+func EditOfficeAddress() gin.HandlerFunc{
+
+}
+
+func DeleteAddress() gin.HandlerFunc{
+	
+}
---
+++ controllers/cart.go
@@ -0,0 +1,21 @@
+package controllers
+
+func AddToCart() gin.HandlerFunc{
+
+}
+
+func RemoveItem() gin.HandlerFunc{
+
+}
+
+func GetItemFromCart() gin.HandlerFunc{
+	
+}
+
+func BuyFromCart() gin.HandlerFunc{
+
+}
+
+func InstantBuy() gin.HandlerFunc{
+
+}
